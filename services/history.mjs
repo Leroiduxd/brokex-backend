b9d2602aa04675ed2188ce554a542eba79e22122
Leroiduxd <89317997+Leroiduxd@users.noreply.github.com>
2025-11-08 20:39:31 +0100

    Update history.mjs

--- services/history.mjs
+++ services/history.mjs
@@ -2,7 +2,7 @@
 import express from "express";
 import fetch from "node-fetch";
 
-const SUPRA_API_KEY = "YOUR_SUPRA_API_KEY_HERE"; // <-- remplace par ta clé
+const SUPRA_API_KEY = "1a2b3c4d5e6f7a8b9c0d1e2f3a4b5c6d7e8f9a0b1c2d3e4f5a6b7c8d9e0f1a2"; // <-- remplace par ta clé
 const REST_BASE = "https://prod-kline-rest.supra.com";
 const HISTORY_BASE = `${REST_BASE}/history`;
 
